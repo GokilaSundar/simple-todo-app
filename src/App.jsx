8b577476f852e253399867ac50e4aae31e12adec
GokilaSundar <gokithilaga267@gmail.com>
2025-03-27 09:01:38 +0530

    Use local storage to persist tasks

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import "./App.css";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [tasks, setTasks] = useState([]);
 
   const [newTask, setNewTask] = useState("");
+
+  // Only if you want to persist the tasks in the local storage
+  useEffect(() => {
+    const tasks = localStorage.getItem("tasks");
+    if (tasks) {
+      setTasks(JSON.parse(tasks));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
 
   return (
     <div className="container">
