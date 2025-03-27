40a799eedc958d3ce16bb2741fb196fff3c05554
GokilaSundar <gokithilaga267@gmail.com>
2025-03-27 08:44:15 +0530

    Implement simple todo list app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,69 @@
 import "./App.css";
 
+import { useState } from "react";
+
 function App() {
-  
+  const [tasks, setTasks] = useState([]);
+
+  const [newTask, setNewTask] = useState("");
+
   return (
-    <>
-      Hello World
-    </>
+    <div className="container">
+      <h1>TODO List</h1>
+      <div className="add-task">
+        <input
+          type="text"
+          value={newTask}
+          onChange={(e) => setNewTask(e.target.value)}
+        />
+        <button
+          disabled={!newTask}
+          onClick={() => {
+            setTasks([
+              ...tasks,
+              {
+                task: newTask,
+                completed: false,
+              },
+            ]);
+            setNewTask("");
+          }}
+        >
+          Add
+        </button>
+      </div>
+      <div className="tasks">
+        {tasks.map((task, index) => (
+          <div
+            key={index}
+            className="task"
+            style={{
+              backgroundColor: task.completed
+                ? "lightgreen"
+                : "lightgoldenrodyellow",
+            }}
+          >
+            <input
+              type="checkbox"
+              checked={task.completed}
+              onClick={() => {
+                const newTasks = [...tasks];
+                newTasks[index].completed = !newTasks[index].completed;
+                setTasks(newTasks);
+              }}
+            />
+            <span>{task.task}</span>
+            <button
+              onClick={() => {
+                setTasks(tasks.filter((_, i) => i !== index));
+              }}
+            >
+              Delete
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
   );
 }
 
